1c73871ce139f62a9d7310a069798f1ba55d6353
Ravi Kumar Vishwakarma <47824408+Ravik27280@users.noreply.github.com>
2022-01-16 19:13:21 +0530

    update Java formating

--- src/com/company/JavaOutputFormat.java
+++ src/com/company/JavaOutputFormat.java
@@ -4,18 +4,11 @@ public class JavaOutputFormat {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("================================");
-        for(int i=0;i<1;i++){
+        for(int i=0;i<3;i++){
             String s1=sc.next();
             int x=sc.nextInt();
+            System.out.printf("%-15s%03d%n",s1,x);
             //Complete this line
-            System.out.print(s1);
-            for(int j=s1.length();j<16;j++){
-                System.out.print(" ");
-            }
-
-            System.out.printf("%4d",x);
-
-
         }
         System.out.println("================================");
 
