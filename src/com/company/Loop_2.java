e9f5b3fdcdf892cef31bd3e77e39f8a77fb6713b
Ravi Kumar Vishwakarma <47824408+Ravik27280@users.noreply.github.com>
2022-01-20 09:50:56 +0530

    update

--- src/com/company/Loop_2.java
+++ src/com/company/Loop_2.java
@@ -2,19 +2,22 @@ package com.company;
 import  java.util.Scanner;
 
 public class Loop_2 {
-    public static void main(String[] args) {
+    public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int t=in.nextInt();
-
+        int sum=0;
         for(int i=0;i<t;i++){
-            int a = in.nextInt(); //0
-            int b = in.nextInt(); //2
-            int n = in.nextInt(); //10
-            int sum=0;
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int temp=1;
             for (int j=1;j<=n;j++){
-                sum=sum+b*j;
-                System.out.println(sum+a);
+                sum=sum+(b*temp);
+                System.out.print(sum+a+" ");
+                temp=temp*2;
             }
+            sum=0;
+            System.out.println();
         }
 
         in.close();
